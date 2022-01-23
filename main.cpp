595882c65086a829aa5f80f7dd5efd4db8eb45fc
IIllIlllIl <wang.tr@outlook.com>
2022-01-23 16:38:09 +0800

    0.0-yamlcpp

---
+++ main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <yaml-cpp/yaml.h>
+#include <json/json.h>
+#include <json/value.h>
+#include <json/writer.h>
+#include <json/reader.h>
+
+int main() {
+    std::cout << "Hello, World!" << std::endl;
+    return 0;
+}
